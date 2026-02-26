4066bb87bb79a92a4fc256d54611eea33f485e6b
Eider Tapasco <eidertapasco.dev@gmail.com>
2026-02-26 14:38:13 -0500

    Initial commit: se creo el proyecto en spring boot con estructura basica.

---
+++ src/main/java/com/bikeshop/rydex/RydexApplication.java
@@ -0,0 +1,13 @@
+package com.bikeshop.rydex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RydexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RydexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikeshop/rydex/RydexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikeshop.rydex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RydexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
